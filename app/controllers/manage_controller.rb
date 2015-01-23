7740ece0d044ac700dfbd64509b28933fb9b51cc
chronicqazxc <chronicqazxc@gmail.com>
2015-01-23 09:16:48 +0800

    .

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -2,12 +2,21 @@ class ManageController < ApplicationController
   def main
     @publishers = Publisher.all
   end
-  def publisher_params
+  def publisher_add_params
     params.require(:publisher).permit(:name, :city, :country)
   end
   def add
-    publisher = Publisher.new(publisher_params)
+    publisher = Publisher.new(publisher_add_params)
     publisher.save
     redirect_to :controller => "main", :action => "welcome"
+  end
+  def publisher_edit_params
+    params.require(:publisher).permit(:id, :name, :city, :country)
+  end
+  def edit
+    logger.debug "#{params}"
+    p = Publisher.find(id: 2)
+    # publisher.update(name: publisher_edit_params[:publisher][:name], city: publisher_edit_params[:publisher][:city], country: publisher_edit_params[:publisher][:country])
+    # redirect_to :controller => "main", :action => "welcome"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,8 @@ Rails.application.routes.draw do
   get 'customer/check_out'
   
   post 'manage/add'
+  
+  post 'manage/edit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
